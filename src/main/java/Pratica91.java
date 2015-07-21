ed2d50d014ee9ffa8db9c4d884c2c66288818821
a1656856 <julio.v.bernardi@hotmail.com>
2015-07-21 08:14:20 -0300

    1

--- src/main/java/Pratica91.java
+++ src/main/java/Pratica91.java
@@ -6,6 +6,8 @@
  * Template de projeto de programa Java usando Maven.
  * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
  */
+
+
 public class Pratica91 {
     public static void main(String[] args) {
         System.out.println(System.getProperty("os.name"));
